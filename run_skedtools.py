92280ec49c61c86191bd5856a84dad474f83f74e
mikegallimore <35549782+mikegallimore@users.noreply.github.com>
2018-03-20 22:33:58 -0400

    Add files via upload

---
+++ run_skedtools.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 22:28:07 2018
+
+@author: Michael
+"""
+
+import parse_append
+import update_tables
+import update_charts
+
+parse_append
+update_tables
+update_charts
